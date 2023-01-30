6744bec4ff56f74658156df9f39de7843ae3e987
Peyton Lamb <prl4538@gmail.com>
2023-01-30 15:43:36 -0600

    edited files 1 and 2 with in class activities

--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,20 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+print(person)
+
+# print out the name of the second child
+print(person["children"][2])
+
+# print out the name of the cat
+print(person["pets"]["cat"])
+
+# itnerate through all children and print out each child
+for i in person["children"]:
+    print(i)
+
+# print out the pets in this format:
+# type of pet: dog name of pet: Fido
+for i, j in person["pets"].items():
+    print(f"type of pet: {i} name of pet: {j}")
