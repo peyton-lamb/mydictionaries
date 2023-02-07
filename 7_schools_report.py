2315b6dce43550dc594a99ad85b8895e219b7fd8
Peyton Lamb <prl4538@gmail.com>
2023-02-06 18:40:21 -0600

    finished schools report

--- 7_schools_report.py
+++ 7_schools_report.py
@@ -17,3 +17,32 @@ Display report for all universities that have a total price for in-state student
 
 
 """
+import json
+
+infile = open('school_data.json', 'r')
+
+schools = json.load(infile)
+conference_schools = [372, 108, 107, 130]
+
+print(type(schools))
+
+#How many schools are in this file?
+print(len(schools))
+
+
+for school in schools:
+    if school["NCAA"]["NAIA conference number football (IC2020)"] in conference_schools:
+        if school["Graduation rate  women (DRVGR2020)"] > 80:
+            print(f"University Name: {school['instnm']}")
+            print(f"Graduation Rate for Women: {school['Graduation rate  women (DRVGR2020)']}")
+            print()
+            print()
+
+for school in schools:
+    if school["NCAA"]["NAIA conference number football (IC2020)"] in conference_schools:
+        if school["Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"] != None and \
+                school["Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)"] > 50000:
+            print(f"University Name: {school['instnm']}")
+            print(f"Total price for in-state students living off campus: ${school['Total price for in-state students living off campus (not with family)  2020-21 (DRVIC2020)']}")
+            print()
+            print()
